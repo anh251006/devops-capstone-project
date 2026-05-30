493dec9638b8fd14d6b8ede41f98cd9be53452f1
Anh Chau <anh.chau@ilikemusic.com>
2026-05-30 16:39:39 -0400

    Added CORS headers

--- service/__init__.py
+++ service/__init__.py
@@ -14,7 +14,10 @@ from service.common import log_handlers
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
-talisman = Talisman(app)
+talisman = Talisman(
+    app,
+    permissions_policy={"browsing-topics": "()"}
+)
 cors = CORS(app)
 
 # Import the routes After the Flask app is created
